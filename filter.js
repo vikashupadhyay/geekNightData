44e5e4da597f4d3991f1b147b1684cd0e3a396a7
piolAtif <spreeti@thoughtworks.com>
2016-10-06 19:12:44 +0530

    [Preeti/Sagar] +Added filter for filtering contents according fields

---
+++ filter.js
@@ -0,0 +1,20 @@
+//output = country name [single ] it will be have array of yes, no , other numbers
+var fs = require('fs');
+var fileText = fs.readFileSync(process.argv[2],'utf-8');
+var lodash = require('lodash');
+
+var columns_indexes = (process.argv).slice(3);
+console.log("Filtering by these indexes =>" ,columns_indexes);
+
+var papaparse = require('papaparse');
+var data = papaparse.parse(fileText).data;
+
+var filterData = function(record){
+	var filterData = [];
+	return lodash.pullAt(record, columns_indexes);
+}
+
+var reducedData = data.map(filterData);
+
+console.log(reducedData);
+
